5f37a26faed587825d02c68ace4a80af13862200
MOe <m.aljaaly@yahoo.co.uk>
2024-11-12 11:03:57 +0000

    done

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
 // import components
-import Pets from '../components/Pets';
-import Hero from '../components/Hero';
-import Services from '../components/Services';
-import Adoption from '../components/Adoption';
-import GeneratePetName from '../components/GeneratePetName';
-import Newsletter from '../components/Newsletter';
-import Footer from '../components/Footer';
+import Pets from "../components/Pets";
+import Hero from "../components/Hero";
+import Services from "../components/Services";
+import Adoption from "../components/Adoption";
+import GeneratePetName from "../components/PetNameGenerator";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
-    <div className='max-w-[1440px] mx-auto overflow-hidden'>
+    <div className="max-w-[1440px] mx-auto overflow-hidden">
       <Hero />
       <Pets />
       <Services />
-      <Adoption />  
-<GeneratePetName />
-  <Newsletter />
+      <Adoption />
+      <GeneratePetName />
+      <Newsletter />
       <Footer />
     </div>
   );
